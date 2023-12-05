7d22f7fa6e869810ae2041a106226f2ec34c1dbc
Jamal077011 <jaime077011@gmail.com>
2023-12-05 10:54:12 +0200

    new

--- resources/views/user/employee/edit.blade.php
+++ resources/views/user/employee/edit.blade.php
@@ -60,8 +60,8 @@
                         <option value="GA">Gaming/Console</option>
                         <option value="PH">Phones</option>
                     </select>
-                </div>
-            </div> -->
+                </div> -->
+            </div>
             <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
                 Update Employee
             </button>
