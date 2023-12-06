db611e78e2ea6f6453dc41981c57bad3ecedd5d4
Jamal077011 <jaime077011@gmail.com>
2023-12-06 10:56:26 +0200

    new

--- resources/views/user/company-files/create.blade.php
+++ resources/views/user/company-files/create.blade.php
@@ -21,7 +21,6 @@
                     <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">File</label>
                     <input type="file" name="file" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Phone Number" required="">
                 </div>
-
             </div>
             <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
                 Add File
