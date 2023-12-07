d7fbd889e28c41cee5673d4fdee377c0334f8e59
hebatallah10 <heba@sketch-tech.com>
2023-12-07 09:09:04 +0200

    validation

--- resources/views/frontend/dashboard/pages/employee/edit.blade.php
+++ resources/views/frontend/dashboard/pages/employee/edit.blade.php
@@ -10,18 +10,30 @@
                 <div class="w-full">
                     <label for="brand" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">English Name</label>
                     <input type="text" name="en_name" value="{{$employer->en_name}}" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="English Name" required="">
+                    @error('en_name')
+                    <small class="text-red-500">{{ $message }}</small>
+                @enderror
                 </div>
                 <div class="w-full">
                     <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Arabic Name</label>
                     <input type="text" name="ar_name" value="{{$employer->ar_name}}" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Arabic Name" required="">
+                    @error('ar_name')
+                    <small class="text-red-500">{{ $message }}</small>
+                @enderror
                 </div>
                 <div class="w-full">
                     <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                     <input type="email" name="email"  value="{{$employer->email}}" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Email" required="">
+                    @error('email')
+                    <small class="text-red-500">{{ $message }}</small>
+                @enderror
                 </div>
                 <div class="w-full">
                     <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
                     <input type="text" value="{{$employer->phone}}" name="phone" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Phone Number" required="">
+                    @error('phone')
+                    <small class="text-red-500">{{ $message }}</small>
+                @enderror
                 </div>
                 <!-- <div>
                     <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company</label>
